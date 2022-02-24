ba9bc73116c7f36e8a858761e1f873a3ba593353
mateuszrog <97434756+mateuszrog@users.noreply.github.com>
2022-02-24 06:13:13 -0500

    Add files via upload

---
+++ test/hello_world_test.py
@@ -0,0 +1,107 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from util.base_command import BaseCommand
+from util.field import Field
+
+
+class HelloWorldTest(unittest.TestCase):
+
+    def setUp(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-extensions")
+        # chrome_options.add_argument("headless")
+
+        s = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
+        self.driver.get("https://www.saucedemo.com/")
+
+        self.base_command = BaseCommand(self.driver)
+
+    def test_sauce_login(self):
+
+        self.base_command.clear_element(Field.username_textfield)
+        self.base_command.send_text_to_element(Field.username_textfield, "standard_user")
+
+        self.base_command.clear_element(Field.username_passwordfield)
+        self.base_command.send_text_to_element(Field.username_passwordfield, "secret_sauce")
+
+        self.base_command.click_element(Field.login_button)
+
+        self.assertEqual("Products".casefold(), self.base_command.get_element_text(Field.product_span).casefold())
+
+        self.base_command.click_element(Field.dropdown)
+        self.base_command.click_element(Field.high_to_low)
+
+        self.base_command.click_element(Field.add_first_to_card)
+        self.base_command.click_element(Field.add_second_to_card)
+        self.base_command.click_element(Field.add_third_to_card)
+
+        self.base_command.click_element(Field.card)
+        time.sleep(3)
+        self.base_command.click_element(Field.checkout)
+
+        self.base_command.clear_element(Field.first_name)
+        self.base_command.send_text_to_element(Field.first_name, "joe")
+
+        self.base_command.clear_element(Field.last_name)
+        self.base_command.send_text_to_element(Field.last_name, "black")
+
+        self.base_command.clear_element(Field.postal_code)
+        self.base_command.send_text_to_element(Field.postal_code, "585858")
+
+        self.base_command.click_element(Field.cont_page)
+
+        self.assertEqual("53.97", self.base_command.get_value(Field.items))
+        self.assertEqual("58.29", self.base_command.get_value(Field.total))
+        self.assertEqual("4.32", self.base_command.get_value(Field.get_tax))
+
+        substract = float(self.base_command.get_value(Field.total)) - float(self.base_command.get_value(Field.items))
+
+        #Substract = round(substract, 1)
+
+        self.assertEqual(substract, float(self.base_command.get_value(Field.get_tax)))
+
+
+        time.sleep(2)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+"""
+        self.base_command.dropdown_price_high_to_low(Field.dropdown)
+        self.base_command.dropdown_price_high_to_low(Field.high_to_low)
+
+        self.base_command.add_to_card(Field.add_first_to_card)
+        self.base_command.add_to_card(Field.add_second_to_card)
+        self.base_command.add_to_card(Field.add_third_to_card)
+
+        self.base_command.go_to_card_and_checkout(Field.card)
+        time.sleep(3)
+        self.base_command.go_to_card_and_checkout(Field.checkout)
+
+        self.base_command.clear_element(Field.first_name)
+        self.base_command.send_informations(Field.first_name, "joe")
+
+        self.base_command.clear_element(Field.last_name)
+        self.base_command.send_informations(Field.last_name, "black")
+
+        self.base_command.clear_element(Field.postal_code)
+        self.base_command.send_informations(Field.postal_code, "585858")
+
+        self.base_command.click_element(Field.cont_page)
+
+        self.assertEqual("53.97", self.base_command.get_value(Field.items))
+        self.assertEqual("58.29", self.base_command.get_value(Field.total))
+        self.assertEqual("4.32", self.base_command.get_value(Field.get_tax))
+"""
---
+++ util/base_command.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import re
+
+
+class BaseCommand:
+    def __init__(self, driver):
+        self.local_driver: webdriver.Chrome = driver
+        self.wait: WebDriverWait = WebDriverWait(driver, 3)
+
+    def click_element(self, element):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).click()
+
+    def clear_element(self, element):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).clear()
+
+    def send_text_to_element(self, element, text):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).send_keys(text)
+
+    def get_element_text(self, element):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, element)))
+        return self.local_driver.find_element(By.XPATH, element).text
+
+    def get_value(self, element):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, element)))
+        Elements = re.findall(r'\d+', self.local_driver.find_element(By.XPATH, element).text)
+        Sum = '.'.join(Elements)
+        return Sum
+
+###########################
+"""
+
+    def dropdown_price_high_to_low(self, element):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).click()
+
+    def add_to_card(self, element):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).click()
+
+    def go_to_card_and_checkout(self, element):
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).click()
+
+    def send_informations(self, element, text):
+        self.wait.until(EC.visibility_of_element_located((By.XPATH, element)))
+        self.local_driver.find_element(By.XPATH, element).send_keys(text)
+
+"""
+
+
+
+
+
+
